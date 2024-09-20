8372df6a7b3ed4bd1527d2970f66ba0e4272b6c3
Balaji B <balaji.balachandar1@gmail.com>
2024-09-20 21:10:39 +0530

    Added functional GUI

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import datetime as dt
 from tkinter import *
 from tkcalendar import Calendar
 from tkinter import messagebox
+import webbrowser # to open new tab in browser with provided in url
+import json # to convert json to dict
 
 load_dotenv("C:/Python/Environmental variables/.env")
 # Below two variables got after creating the account
@@ -12,25 +14,37 @@ PIXELA_TOKEN = os.getenv("pixela_token")
 PIXELA_USER = os.getenv("pixela_user")
 pixela_url = "https://pixe.la/v1/users"
 
+def format_date():
+    cal.config(date_pattern="yyyyMMdd")
+    date = cal.get_date()
+    # cal.config(date_pattern="dd/MM/yyyy")
+    return date
+
 def reset_pixel():
-    reset_url = f"{pixela_url}/{PIXELA_USER}/graphs/graph1/{formatted_date}"
+    reset_url = f"{pixela_url}/{PIXELA_USER}/graphs/graph1/{format_date()}"
     response = requests.delete(url=reset_url,headers=headers)
     print(response.text)
     messagebox.showinfo(title="Reset/Delete Pixel",message="Reset Successful.")
 
 def add_pixel():
     post_url = f"{pixela_url}/{PIXELA_USER}/graphs/graph1"
+    input_field = hours_spent.get()
     pixel_parameters = {
-        "date":formatted_date,
-        "quantity":hours_spent
+        "date":format_date(),
+        "quantity":input_field
     }
-    if hours_spent is None:
-        messagebox.showinfo(title="Error", message="Field is empty.")
-    elif hours_spent is int:
-        response = requests.post(url=post_url,json=pixel_parameters,headers=headers)
-        messagebox.showinfo(title="Post", message="Posted Successful.")
-        print(response.text)
+    response = requests.post(url=post_url, json=pixel_parameters, headers=headers)
+    data = json.loads(response.text)
+    is_success = data["isSuccess"]
+    hours_spent.delete(0,END)
 
+    if is_success:
+        messagebox.showinfo(title="Post", message="Posted Successful.")
+    else:
+        messagebox.showinfo(title="Error", message="Field is empty (or) \nInvalid input (or) \nAPI rejects 25% times, so TRY AGAIN...")
+
+def journey():
+    webbrowser.open(url="https://pixe.la/v1/users/balaji3/graphs/graph1.html")
 # Creating an account--------------------------------------------------------------------------
 # the token will be what we type in the token field below
 # parameters = {
@@ -70,7 +84,7 @@ headers = {
 
 # Creating a pixel-------------------------------------------------------------------------------------------------
 raw_date = dt.datetime.now()
-# formatted_date = raw_date.strftime("%Y%m%d")
+formatted_date = raw_date.strftime("%Y%m%d")
 # pixel_parameters = {
 #     "date":formatted_date,
 #     "quantity":input("Hours Python Studied Today --- ")
@@ -81,6 +95,7 @@ raw_date = dt.datetime.now()
 window = Tk()
 window.title("Python Journey")
 window.config(padx=20,pady=20,bg = "#4B8BBE")
+window.resizable(width=False,height=False)
 
 today_date = int(raw_date.strftime("%d"))
 this_month = int(raw_date.strftime("%m"))
@@ -91,27 +106,24 @@ cal = Calendar(selectmode = 'day',
                day=today_date,
                month=this_month,
                year=this_year)
-user_date_input = cal.get_date()
-
-print(user_date_input)
-cal.grid(row=0,column = 0,columnspan = 4,pady = 10)
+cal.grid(row=0,column = 0,columnspan = 3)
 
 text = Label(text="Hours Spent:",bg="#646464",fg="white")
-text.grid(row=1, column=0, columnspan=2, pady=10, sticky="e")
+text.grid(row=1, column=0,pady = 20,sticky = "e")
 
 hours_spent = Entry(width=10)
-hours_spent.grid(row=1, column=2, sticky="w")
+hours_spent.grid(row=1, column=1,pady = 20,sticky = "w")
 
-reset = Button(text="Reset",command=reset_pixel)
-reset.grid(row = 3, column = 0,pady=10)
+reset = Button(text="Reset",command=reset_pixel,width=10)
+reset.grid(row = 2, column = 0,sticky = "e",padx = 10)
 
-update = Button(text="Update")
-update.grid(row = 3, column = 1,sticky = "w",pady=10)
+# update = Button(text="Update")
+# update.grid(row = 3, column = 1,sticky = "w",pady=10)
 
-post = Button(text="Post",command=add_pixel)
-post.grid(row = 3, column = 2,sticky = "w",pady=10)
+post = Button(text="Post",command=add_pixel,width=10)
+post.grid(row = 2, column = 1,sticky = "w")
 
-show_info = Button(text="Show\nJourney")
-show_info.grid(row = 3, column = 3)
+show_info = Button(text="Show\nJourney",command=journey)
+show_info.grid(row = 2, column = 2)
 window.mainloop()
 
